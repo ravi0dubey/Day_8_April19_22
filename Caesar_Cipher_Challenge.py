94aa4a765cc2b22014c275189051d852a607e30b
Ravi Dubey <38419795+ravi0dubey@users.noreply.github.com>
2022-04-20 02:53:29 +0000

    encrypt_message

--- Caesar_Cipher_Challenge.py
+++ Caesar_Cipher_Challenge.py
@@ -1,21 +1,33 @@
 def encrypt_message(message_text,shift_pos):
-  input_message = []
-  pos = 0
-  blank = " "
   char_set = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-  for i in range(0,len(message_text)):
-    input_message.insert(i,blank)
- 
+  cipher_text = " "
+
+#approach 1  
+  #input_message = []
+  #blank = " "  
+  #for i in range(0,len(message_text)):
+  # input_message.insert(i,blank)
       
-  for i in range(0,len(message_text)):
-    for j in range(0,len(char_set)):
-      if(message_text[i] == char_set[j]):
-        
-        input_message[i]= char_set[j+shift_pos]
-        break
-      
-  
-  print(input_message)
+  #for i in range(0,len(message_text)):
+  # for j in range(0,len(char_set)):
+  #    if(message_text[i] == char_set[j]):
+  #      if(j+shift_pos > 26):
+  #        input_message[i]= char_set[j+shift_pos - 26]
+  #      else:
+  #        input_message[i]= char_set[j+shift_pos]
+  #      break
+  #print(input_message)
+
+#approach 2  
+  for char in message_text:
+    pos = char_set.index(char)
+    if(pos+shift_pos > 26):
+      encrypt_letter = char_set[pos+shift_pos - 26]
+    else:
+      encrypt_letter = char_set[pos+shift_pos]
+    cipher_text += encrypt_letter 
+    
+  print(cipher_text)
 
 
 
@@ -23,4 +35,5 @@ direction= input("Type 'encode' to encrypt, type 'decode'to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-encrypt_message(text,shift)
+if (direction == "encode"):
+  encrypt_message(text,shift)
